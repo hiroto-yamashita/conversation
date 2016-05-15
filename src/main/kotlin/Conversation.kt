8395f4fc2c979fbc06645f05cb68f6189d7f6c43
hiroto-yamashita <hy@oisix.co.jp>
2016-05-15 19:03:04 +0900

    separate cart and products

--- src/main/kotlin/Conversation.kt
+++ src/main/kotlin/Conversation.kt
@@ -13,42 +13,55 @@ class Conversation {
 			println(input)
 			var tokenizer = Tokenizer()
 			var tokens = tokenizer.tokenize(input)
-			var isCart = false
+			var isproducts = false
 			var contents = false
+			var cartadd = false
+			var products : MutableList<String> = mutableListOf()
 			for (token in tokens) {
 				println("${token.getSurface()}\t${token.getAllFeatures()}")
 				var features = token.getAllFeaturesArray()
 				if (features.size == 9) {
 					if (features[0] == "名詞") {
 						if (features[7] == "カゴ") {
-							isCart = true
+							isproducts = true
 						}
 						if (features[7] == "ボックス") {
-							isCart = true
+							isproducts = true
 						}
 						if (features[7] == "カート") {
-							isCart = true
+							isproducts = true
 						}
-					}
-					if (features[7] == "ナカミ") {
-						contents = true
-					}
-					if (features[7] == "トマト") {
-						var quantity = cart.get("トマト")
-						if (quantity == null) quantity = 0
-						cart.put("トマト", ++quantity)
-					}
-					if (features[7] == "メロン") {
-						var quantity = cart.get("メロン")
-						if (quantity == null) quantity = 0
-						cart.put("メロン", ++quantity)
+						if (features[7] == "ナカミ") {
+							contents = true
+						}
+						if (features[7] == "トマト") {
+							products.add("トマト")
+						}
+						if (features[7] == "メロン") {
+							products.add("メロン")
+						}
 					}
 					if (features[6] == "終わる" || features[6] == "おわる") {
 						end = true
 					}
+					if (features[6] == "くださる") {
+						cartadd = true
+					}
 				}
 			}
-			if (isCart) {
+			if (cartadd) {
+				if (!products.isEmpty()) {
+					products.forEach { 
+						var quantity = cart.get(it)
+						if (quantity == null) quantity = 0
+						cart.put(it, ++quantity)
+						println("${it}をカートに入れました")
+					}
+				} else {
+					println("何がほしいですか？")
+				}
+			}
+			if (isproducts) {
 				if (contents) {
 					println("定期ボックスには")
 					cart.forEach { println(it) }
